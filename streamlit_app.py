bd38812a4ffe02b951b9db754d0135ff93160ecb
alexpowys <125526202+alexpowys@users.noreply.github.com>
2024-10-03 08:13:36 +0000

    initial commit v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,41 @@
 import streamlit as st
+import snowflake.connector
+import pandas as pd
 
-st.title("🎈 My new app")
+
+# Write directly to the app
+st.title("Active Staff Training Counts")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    """Counts
+    """
 )
+
+conn = snowflake.connector.connect(
+        user='DEPUTYUSER',
+        password='wm$4Zh62',
+        account='dfzqycb-tz75056',
+        warehouse='COMPUTE_WH',
+        database='DEPUTY',
+        schema='FACT')
+
+cursor = conn.cursor()
+
+query = '''
+select * from mod_commission
+qualify cycle_num = max(cycle_num) over()
+order by total_commission desc;
+'''
+
+cursor.execute(query)
+result = cursor.fetchall()
+
+df = pd.DataFrame(result,
+                      columns=['MoD', 'Cycle', 'Commission Type', 'Total Sales', 'Total Commission']).sort_values(by='Total Commission')
+
+# Create a simple bar chart
+# See docs.streamlit.io for more types of charts
+st.subheader("MoD Commission")
+st.bar_chart(data=df, x="MoD", y = "Total Commission")
+
+st.subheader("Underlying data")
+st.dataframe(df, use_container_width=True)
